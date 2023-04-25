28c54f6655676939d9a7b62ce7087f91b5cc886c
ALABSSON <yusufolatunjialabi@gmail.com>
2023-04-25 21:18:10 +0100

    Second commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../src/CSS/DeskView.css'
+import '../src/CSS/MobileView.css'
+
+const App = () => {
+  return (
+    <div>
+      New react
+    </div>
+  )
+}
+
+export default App
